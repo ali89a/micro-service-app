e4119f4ea17733068adaa9bfb88d0f882e1a11c7
Mohammad Ali <mdali89a@gmail.com>
2022-07-22 14:51:58 +0600

    date picker added admin index

--- resources/views/admin/access_control/user/index.blade.php
+++ resources/views/admin/access_control/user/index.blade.php
@@ -1,14 +1,20 @@
 @extends('admin.layouts.app')
 
 @section('title', 'Admin List')
+@push('style')
+<link rel="stylesheet" type="text/css" href="{{asset('admin/app-assets/vendors/css/pickers/pickadate/pickadate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('admin/app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('admin/app-assets/css/plugins/forms/pickers/form-flat-pickr.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('admin/app-assets/css/plugins/forms/pickers/form-pickadate.css')}}">
+@endpush
 @section('content')
 
 <div class="content-wrapper">
     @php
-        $links = [
-            'Home'=>route('admin.dashboard'),
-            'Admin list'=>''
-        ]
+    $links = [
+    'Home'=>route('admin.dashboard'),
+    'Admin list'=>''
+    ]
     @endphp
     <x-bread-crumb-component title='Admin list' :links="$links" />
     <div class="content-body">
@@ -27,8 +33,22 @@
                     </div>
 
                     <div class="card-body table-responsive">
+                        <div class="row justify-content-center">
+                            <div class="col-md-3 form-group">
+                                <label for="from_date">From Date</label>
+                                <input type="text" id="from_date" class="form-control flatpickr-basic" placeholder="YYYY-MM-DD" />
+                            </div>
+                            <div class="col-md-3 form-group">
+                                <label for="fp-default">To Date</label>
+                                <input type="text" id="fp-default" class="form-control flatpickr-basic" placeholder="YYYY-MM-DD" />
+                            </div>
+                            <div class="col-md-3 form-group">
+                                <label for="fp-default"></label>
+                                <button class="btn btn-primary form-control" type="submit">Search</button>
+                            </div>
+                        </div>
                         <table id="dataTable" class="datatables-basic table table-bordered table-secondary table-striped">
-                            {{--  show from datatable--}}
+                            {{-- show from datatable--}}
                         </table>
                     </div>
                 </div>
@@ -39,24 +59,65 @@
 </div>
 @endsection
 @push('script')
-    <script>
-        $(document).ready(function () {
-            $('#dataTable').dataTable({
-                stateSave: true,
-                responsive: true,
-                serverSide: true,
-                processing: true,
-                ajax: '{{ route('admin.admin.index') }}',
-                columns: [
-                    {data: "DT_RowIndex",title:"SL", name: "DT_RowIndex", searchable: false, orderable: false},
-                    {data: "name", title:"Name", searchable: true},
-                    {data: "email", title:"Email", searchable: true},
-                    {data: "mobile", title:"Mobile", searchable: true, "defaultContent": '<i class="text-danger">Not set</i>'},
-                    {data: "role_info", title:"Role", searchable: true, "defaultContent": '<i class="text-danger">Not set</i>'},
-                    {data: "status",title:"Status", orderable: false, searchable: false},
-                    {data: "action",title:"Action", orderable: false, searchable: false},
-                ],
-            });
-        })
-    </script>
+
+
+<script src="{{asset('admin/app-assets/vendors/js/pickers/pickadate/picker.js')}}"></script>
+<script src="{{asset('admin/app-assets/vendors/js/pickers/pickadate/picker.date.js')}}"></script>
+<script src="{{asset('admin/app-assets/vendors/js/pickers/pickadate/picker.time.js')}}"></script>
+<script src="{{asset('admin/app-assets/vendors/js/pickers/pickadate/legacy.js')}}"></script>
+<script src="{{asset('admin/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js')}}"></script>
+<script src="{{asset('admin/app-assets/js/scripts/forms/pickers/form-pickers.js')}}"></script>
+<script>
+    $(document).ready(function() {
+        $('#dataTable').dataTable({
+            stateSave: true,
+            responsive: true,
+            serverSide: true,
+            processing: true,
+            ajax: '{{ route('admin.admin.index') }}',
+            columns: [{
+                    data: "DT_RowIndex",
+                    title: "SL",
+                    name: "DT_RowIndex",
+                    searchable: false,
+                    orderable: false
+                },
+                {
+                    data: "name",
+                    title: "Name",
+                    searchable: true
+                },
+                {
+                    data: "email",
+                    title: "Email",
+                    searchable: true
+                },
+                {
+                    data: "mobile",
+                    title: "Mobile",
+                    searchable: true,
+                    "defaultContent": '<i class="text-danger">Not set</i>'
+                },
+                {
+                    data: "role_info",
+                    title: "Role",
+                    searchable: true,
+                    "defaultContent": '<i class="text-danger">Not set</i>'
+                },
+                {
+                    data: "status",
+                    title: "Status",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "action",
+                    title: "Action",
+                    orderable: false,
+                    searchable: false
+                },
+            ],
+        });
+    })
+</script>
 @endpush
